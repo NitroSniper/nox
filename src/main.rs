9967bc0b195e4b7b5b5feb8a6f0ea6e8762d0491
NitroSniper <mahiemiah35@gmail.com>
2024-08-22 00:19:27 +0100

    learning basics of clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use clap::{Parser, Subcommand, ValueEnum};
+
+/// A Nix tool to quickstart development and packaging applications
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Enter a development shell of chosen language
+    Enter {
+        /// Language to be chosen
+        #[arg(value_enum)]
+        lang: Language,
+    },
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
+enum Language {
+    Rust,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.command {
+        Commands::Enter { lang } => {
+            println!("You suck at {:?}", lang);
+        }
+    }
 }
